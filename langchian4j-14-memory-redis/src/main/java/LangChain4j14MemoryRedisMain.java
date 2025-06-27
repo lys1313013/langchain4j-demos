2520a3515946b3fa12bab0510c3bf3100896be6d
lys1313013 <lys1313013@gmail.com>
2025-06-27 23:44:06 +0800

    feat： 14 记忆持久化到redis

---
+++ langchian4j-14-memory-redis/src/main/java/LangChain4j14MemoryRedisMain.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类功能描述
+ *
+ * @author Lys
+ * @date 2025/06/27 23:40
+ */
+@SpringBootApplication
+public class LangChain4j14MemoryRedisMain {
+    public static void main(String[] args) {
+        SpringApplication.run(LangChain4j14MemoryRedisMain.class, args);
+    }
+}
